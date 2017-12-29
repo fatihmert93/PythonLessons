175a473c54ea4461d651ef41d3c3d3614a6a5955
fatihmert93 <fatihmert93@gmail.com>
2017-12-29 17:54:20 +0300

    spell checker eklendi.

--- src/notepad.py
+++ src/notepad.py
@@ -1,7 +1,9 @@
 import sys
 import os
 
-from PyQt5.QtWidgets import QWidget,QApplication,QTextEdit,QLabel,QPushButton,QVBoxLayout,QHBoxLayout,QFileDialog
+from PyQt5.QtWidgets import QWidget,QApplication,QTextEdit,QLabel,QPushButton,QVBoxLayout,QHBoxLayout,QFileDialog,QMainWindow,QAction,qApp
+
+
 
 class Notepad(QWidget):
     def __init__(self):
@@ -35,8 +37,6 @@ class Notepad(QWidget):
         self.btn_save.clicked.connect(self.save_file)
         self.btn_open.clicked.connect(self.open_file)
 
-        self.show()
-
 
 
     def clean_text(self):
@@ -53,11 +53,58 @@ class Notepad(QWidget):
         with open(filename[0],"w") as file:
             file.write(self.textarea.toPlainText())
 
+class Menu(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.notepad = Notepad()
+
+        self.setCentralWidget(self.notepad)
+
+        self.create_menus()
+
+    def create_menus(self):
+        menubar = self.menuBar()
+        file = menubar.addMenu("File")
+
+        action_openfile = QAction("Open File",self)
+        action_openfile.setShortcut("Ctrl+O")
+
+        action_savefile = QAction("Save File", self)
+        action_savefile.setShortcut("Ctrl+S")
+
+        action_clean = QAction("Clean", self)
+        action_clean.setShortcut("Ctrl+D")
+
+        action_quit = QAction("Quit", self)
+        action_quit.setShortcut("Ctrl+Q")
+
+        file.addAction(action_savefile)
+        file.addAction(action_openfile)
+        file.addAction(action_clean)
+        file.addAction(action_quit)
+
+        file.triggered.connect(self.response)
+
+        self.setWindowTitle("Text Editor")
+        self.show()
+
+    def response(self,action):
+        if(action.text() == "Open File"):
+            self.notepad.open_file()
+        elif(action.text() == "Save File"):
+            self.notepad.save_file()
+        elif (action.text() == "Clean"):
+            self.notepad.clean_text()
+        elif (action.text() == "Quit"):
+            qApp.quit()
+
 
 
 app = QApplication(sys.argv)
 
-notepad = Notepad()
+menu = Menu()
 
 sys.exit(app.exec_())
 
